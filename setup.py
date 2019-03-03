8a4c9969d2a2d7e0725b74b637de6f3376050db7
bot <miss.sherry.cherish@gmail.com>
2019-03-03 18:02:44 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='prettyprint',
+    version='0.0',
+
+    url='https://github.com/sherry255/prettyprint',
+    description='prettyprint',
+    license='BSD',
+
+    classifiers=[
+        "Development Status:: 5 - Production / Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System:: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    author='sherry',
+    author_email='xcmkya@gmail.com',
+
+    packages=['prettyprint'],
+    zip_safe=False,
+)
